77779337628f56f43e5830fc23f2d2e1717124ae
Eyüp Halit İnci <54631883+hypermorire@users.noreply.github.com>
2021-03-26 11:23:07 +0300

    SupplySignal

---
+++ Config.cs
@@ -0,0 +1,25 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace SupplySignal
+{
+    public class Config : IRocketPluginConfiguration
+    {
+        public bool SupplyAnnounce;
+        public string SupplyPermission, AnnouncMessage, AnnouncImgUrl;
+        [XmlArrayItem("SignalIDs")]
+        public List<ushort> SignalSmokes { get; set; } = new List<ushort>();
+        public void LoadDefaults()
+        {
+            SupplyAnnounce = true;
+            SupplyPermission = "hyper.supply";
+            AnnouncMessage = "{color=yellow}%PLAYERNAME%{/color} {color=white}called air drop.{/color}";
+            AnnouncImgUrl = "";
+            SignalSmokes = new List<ushort>
+            {
+                261,
+            };
+        }
+    }
+}
